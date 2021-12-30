c19ba8d47cea466648785420d09dcf88350f9d37
Aileen Olivares <aca.olivares@gmail.com>
2021-12-30 13:30:59 -0300

    botones en cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,12 @@ else{
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: "navbarSupportedContent",
 });
+
+
+// tooltip en destacados button de información
+
+var tooltipTriggerList =
+[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
